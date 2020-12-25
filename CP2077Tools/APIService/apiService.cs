030069b8e62f8fc2b3bec5fae83377757cf9f603
3DMXM <xiaomo1433675239@outlook.com>
2020-12-25 15:43:40 +0800

    1

---
+++ CP2077Tools/APIService/apiService.cs
@@ -0,0 +1,182 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetDimension.NanUI.DataServiceResource;
+using NetDimension.NanUI.ResourceHandler;
+using CP77Tools.Tasks;
+using WolvenKit.Common.Tools.DDS;
+using CP2077Tools;
+using System.IO;
+using WolvenKit.Common.Services;
+using CP77.CR2W.Archive;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cyberpunk2077Tools.Service
+{
+    // api.app.local/api
+    class apiService : DataService
+    {
+
+        public string[] archivePath;
+
+        //public Archive ar;
+
+        // api.app.local/api/hi
+        public ResourceResponse Hi(ResourceRequest request)
+        {
+            return Text("Hello NanUI!");
+        }
+
+        public ResourceResponse closeApp(ResourceRequest request)
+        {
+
+            System.Environment.Exit(0);   //这是最彻底的退出方式，不管什么线程都被强制退出，把程序结束的很干净，退出时有可能会抛出异常  
+            return Json(new { code = "00", msg = "关闭完成" });
+        }
+        // 选择文件 获取列表
+        public ResourceResponse SelectFile(ResourceRequest request)
+        {
+
+            string[] file = request.UploadFiles;  //选择的文件路径
+
+            if (file != null)
+            {
+                archivePath = file;
+                //var fileList = getGameFileList(file[0]); // 获取 archive文件中的文件列表
+                //return Json(new { code = "00", filePath = file, fileList = fileList });
+                return Json(new { code = "00", filePath = file });
+
+                //var fileList = new JObject {  };
+                //return Json(new { code = "00", filePath = file, fileList = fileList });
+            }
+            else
+            {
+                return Json(new { code = "99", msg = "文件路径为空" });
+            }
+        }
+
+        // 导出文件
+        public ResourceResponse ExtractSelectedFile(ResourceRequest request)
+        {
+
+            var PFileData = ((JObject)JsonConvert.DeserializeObject(request.StringContent)).ToObject<PostFileData>();   //接收并转换传递过来的文件格式
+
+            string outpath = PFileData.ExtractFolder;
+
+            if (outpath == null) outpath = ""; 
+
+            foreach (var FileHash in PFileData.FileHashList)
+            {
+                ExtractFileToFolder(PFileData.FilePath, outpath, FileHash, false);
+            }
+            return Json(new { code = "00", msg = "文件导出完成" });
+        }
+        // 导出所有文件
+        public ResourceResponse ExtractAllFile(ResourceRequest request)
+        {
+
+            var PFileData = ((JObject)JsonConvert.DeserializeObject(request.StringContent)).ToObject<PostFileData>();   //接收并转换传递过来的文件格式
+
+            string outpath = PFileData.ExtractFolder;
+
+            if (outpath == null) outpath = "";
+
+            foreach (var File in PFileData.FileList)
+            {
+                ExtractFileToFolder(File, outpath, 0x00, true);
+            }
+
+            return Json(new { code = "00", msg = "完毕" });
+        }
+
+        // 获取 .archive 文件中的所有文件列表
+        private List<FileListType> getGameFileList(string filePath)
+        {
+            //DirectoryInfo outDir;
+            var inputFileInfo = new FileInfo(filePath);
+            var inputDirInfo = new DirectoryInfo(filePath);
+
+            var tobeprocessedarchives = inputFileInfo.Exists
+                    ? new List<FileInfo> { inputFileInfo } :
+                    inputDirInfo.GetFiles().Where(_ => _.Extension == ".archive");
+
+            List<FileListType> FileList = new List<FileListType>();
+            foreach (var processedarchive in tobeprocessedarchives)
+            {
+                var ar = new Archive(processedarchive.FullName);
+                foreach (var entry in ar.Files)
+                {
+                    FileListType FLT = new FileListType();
+                    FLT.FileValue = entry.Value.NameStr;
+                    FLT.FileHash = entry.Value.NameHash64;
+                    FLT.FileType = Logtype.Normal;
+                    FileList.Add(FLT);
+
+                }
+            }
+
+            return FileList;
+        }
+
+        // 提取文件到文件夹
+        private void ExtractFileToFolder(string file, string outpath, ulong hash, bool allFile)
+        {
+            // get outdirectory
+            DirectoryInfo outDir;
+
+            //var inputDirInfo = new DirectoryInfo(file);
+            FileInfo inputFileInfo = new FileInfo(file);
+
+            if (outpath == null || outpath == "" )
+            {
+                outpath = inputFileInfo.DirectoryName;
+            }
+
+            outDir = new DirectoryInfo(outpath);
+            if (!outDir.Exists)
+            {
+                outDir = Directory.CreateDirectory(outpath);
+            }
+
+            outDir = Directory.CreateDirectory(Path.Combine(
+                   outDir.FullName,
+                   inputFileInfo.Name.Replace(".archive", "")));
+
+            var ar = new Archive(file);
+
+            if (ar == null) return;
+
+            if (allFile)
+            {
+                ar.ExtractAll(outDir, null, null);
+            }
+            else
+            {
+                ar.ExtractSingle(hash, outDir); // 提取单个文件到指定目录
+            }
+        }
+
+    }
+
+
+    public class FileListType
+    {
+        public string FileValue { get; set; }
+        public Logtype FileType { get; set; }
+        public ulong FileHash { get; set; }
+    }
+
+    public class PostFileData
+    {
+        public List<ulong> FileHashList { get; set; }
+        public List<string> FileList { get; set; }
+        public string ExtractFolder { get; set; }
+        public string FilePath { get; set; }
+    }
+}
---
+++ CP2077Tools/wwwroot/index.js
@@ -0,0 +1,312 @@
+﻿var api = "http://api.app.local";
+$(document).on({    // 禁用游览器默认行为
+    dragleave:function(e){      //拖离
+        e.preventDefault();
+    },
+    drop:function(e){           //拖后放
+        e.preventDefault();
+    },
+    dragenter:function(e){      //拖进
+        e.preventDefault();
+    },
+    dragover:function(e){       //拖来拖去
+        e.preventDefault();
+    }
+});
+
+$(document).ready(function () {
+    $(".testBtn").click(function () {
+        $.get(api+"/api/hi",function (red) {
+            console.log(red);
+        })
+    });
+    // 隐藏
+    $(".hide").click(function () {
+        var type = $(this).attr('type');
+        if (type == "toHide"){
+            $(".container").fadeOut();
+            $(this).attr('type',"toShow");
+
+            $(".toShow").show();
+            $(".toHide").hide();
+        }else {
+            $(".container").fadeIn();
+            $(this).attr('type',"toHide");
+
+            $(".toHide").show();
+            $(".toShow").hide();
+        }
+    });
+    // 刷新
+    $(".refresh").click(function () {
+        location.reload();
+    });
+    // 关闭
+    $(".close").click(function () {
+        if (confirm("您确定要关闭吗?如果有在正在解包的文件,将被停止")){
+            $.post(api + "/api/closeApp",function (red) {
+                console.log(red);
+            });
+        }
+
+    });
+    // 选择文件
+    $(".Select-file").change(function () {
+        var file = $(this).prop('files');
+        // var FilePathList = [];
+        StartLoading()
+        for (var i = 0; i < file.length; i++){
+            var data = new FormData();
+            data.append('file', file[i]);
+            var aj = $.ajax({
+                url:api+"/api/SelectFile",
+                type: 'POST',
+                data: data,
+                processData : false,
+                async: false,
+                dataType: "json",
+                success:function (red) {
+                    // 返回的数据
+                    if (red.code == "00"){
+                        Stoploading();
+                        $("#file-path").val(red.filePath[0]);
+                        if (i>0){
+                            showFileList2(file[i],red.filePath[0],false);
+                        }else {
+                            showFileList2(file[i],red.filePath[0]);
+                        }
+                    }else {
+                        alert(red.msg)
+                    }
+                }
+            });
+        }
+
+    })
+    // 拖拽区域
+    var box = document.getElementById('area');
+    box.addEventListener("drop",
+        function(e) {
+            e.preventDefault(); //取消默认浏览器拖拽效果
+            var fileList = e.dataTransfer.files; //获取文件对象
+            //检测是否是拖拽文件到页面的操作
+            if (fileList.length == 0) {
+                return false;
+            }
+            //检测文件格式是否正确
+            if (fileList[0].name.indexOf('.archive') === -1) {
+                alert("请选择.archive文件");
+                return false;
+            }
+            StartLoading() ;    // 显示加载动画
+            for (var i = 0; i < fileList.length;i++){
+                var data = new FormData();
+                data.append('file', fileList[i]);
+                var FilePath = "";
+                var aj = $.ajax({
+                    url:api+"/api/SelectFile",
+                    type: 'POST',
+                    data: data,
+                    processData : false,
+                    async: false,
+                    dataType: "json",
+                    success:function (red) {
+                        // 返回的数据
+                        Stoploading();  // 停止加载动画
+                        // $("#file-path").val(red.filePath[0]);
+                        // FilePath = red.filePath[0];
+                        // showFileList2(fileList,FilePath);
+                        if (i>0){
+                            showFileList2(fileList[i],red.filePath[0],false);
+                        }else {
+                            showFileList2(fileList[i],red.filePath[0]);
+                        }
+                    }
+                });
+            }
+        },
+        false);
+
+    // 提取选中的文件
+    $(".ExtractSelectedFile").click(function () {
+        if (!CheckFile()){
+            return;
+        }
+        var fileList = [];
+        var FilePath = $("#file-path").val();
+        $(".file-list-ltem-checkbox:checked").each(function() {
+            // fileList.push($(this).attr("file-hash"));
+            fileList.push($(this).attr("file-path"));
+        });
+        var ExtractFolder = $(".export-path-text").val();
+        var data = {
+            FileList:fileList,
+            ExtractFolder:ExtractFolder,
+            FilePath:FilePath,
+        }
+        $.post(api+"/api/ExtractAllFile",JSON.stringify(data),function (red) {
+            console.log(red)
+        })
+    });
+    // 提取全部文件
+    $(".ExtractAllFile").click(function () {
+        if (!CheckFile()){
+            return;
+        }
+
+        var fileList = [];
+        var FilePath = $("#file-path").val();
+
+        $(".file-list-ltem-checkbox").each(function() {
+            // fileList.push($(this).attr("file-hash"));
+            fileList.push($(this).attr("file-path"));
+        });
+
+        var ExtractFolder = $(".export-path-text").val();
+        var data = {
+            FileList:fileList,
+            ExtractFolder:ExtractFolder,
+            FilePath:FilePath,
+        }
+        $.post(api+"/api/ExtractAllFile",JSON.stringify(data),function (red) {
+            console.log(red);
+        });
+    });
+});
+
+
+let getUrl = function(flie) {
+    let url = ''
+    if (window.createObjectURL!=undefined) { // basic
+        url = window.createObjectURL(flie) ;
+    }else if (window.webkitURL!=undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(flie) ;
+    }else if (window.URL!=undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(flie) ;
+    }
+    return url
+}
+
+//  格式化文件大小
+function renderSize(value){
+    if(null==value||value==''){
+        return "0 Bytes";
+    }
+    var unitArr = new Array("Bytes","KB","MB","GB","TB","PB","EB","ZB","YB");
+    var index=0,
+        srcsize = parseFloat(value);
+    index=Math.floor(Math.log(srcsize)/Math.log(1024));
+    var size =srcsize/Math.pow(1024,index);
+    //  保留的小数位数
+    size=size.toFixed(2);
+    return size+unitArr[index];
+}
+
+function showFileList2(fileList,FilePath,clearEmt = true) {
+    var html = "";
+    var fileListItem = "";
+    // for (var i = 0; i < fileList.length ; i++) {
+        var FileData = fileList;
+        fileListItem += "<li class=\"file-list-item ripple\">" +
+            "   <label>" +
+            "       <input type='checkbox' name='fileList' class='file-list-ltem-checkbox' file-path='" + FilePath + "'>" +
+            "       <div class='file-list-ltem-data'>" +
+            "           <div class=\"file-list-item-name\">" + FileData.name + "</div>" +
+            "           <div class=\"file-list-item-size\">" + renderSize(FileData.size) + "</div>" +
+            "       </div>" +
+            "    </label>" +
+            "</li>";
+    // }
+    html += " <div class=\"file-list\">" +
+        "     <li class=\"file-list-item th\">" +
+        "       <div class='file-list-ltem-th-checkbox' style='text-align: center'>选择</div>"+
+        "       <div class='file-list-ltem-data'>"+
+        "           <div class=\"file-list-item-name\">文件</div>" +
+        "           <div class=\"file-list-item-size\" style='text-align: center'>大小</div>" +
+        "       </div>" +
+        "     </li>" +
+                fileListItem +
+        "     </div>";
+
+    // 判断是清空还是添加
+    if (clearEmt) {
+        var nexmoeItem = $(".file-list-nexmoe");
+        nexmoeItem.empty();
+        nexmoeItem.append(html);
+    }else {
+        var fileList = $(".file-list");
+        fileList.append(fileListItem);
+    }
+}
+
+// 显示文件列表
+// function showFileList(fileList) {
+//     var html = "";
+//     var fileListItem = "";
+//     for (var i = 0; i < fileList.length ; i++){
+//         var FileData = fileList[i];
+//         fileListItem += "<li class=\"file-list-item ripple\">" +
+//             "   <label>"+
+//             "       <input type='checkbox' name='fileList' class='file-list-ltem-checkbox' file-hash='"+FileData.FileHash+"'>"+
+//             "       <div class='file-list-ltem-data'>" +
+//             "           <div class=\"file-list-item-name\">"+FileData.FileValue+"</div>" +
+//             "           <div class=\"file-list-item-size\">"+renderSize(FileData.size)+"</div>" +
+//             "       </div>" +
+//             "    </label>"+
+//             "</li>" ;
+//     }
+//     html += " <div class=\"file-list\">" +
+//         "     <li class=\"file-list-item th\">" +
+//         "       <div class='file-list-ltem-th-checkbox' style='text-align: center'>选择</div>"+
+//         "       <div class='file-list-ltem-data'>"+
+//         "           <div class=\"file-list-item-name\">文件</div>" +
+//         "           <div class=\"file-list-item-size\" style='text-align: center'>大小</div>" +
+//         "       </div>" +
+//         "     </li>" +
+//               fileListItem +
+//         "     </div>";
+//     var nexmoeItem = $(".file-list-nexmoe");
+//     nexmoeItem.empty();
+//     nexmoeItem.append(html);
+// }
+
+//显示加载页面
+function StartLoading() {
+    $(".loding").fadeIn();
+}
+// 隐藏加载页面
+function Stoploading() {
+    $(".loding").fadeOut();
+}
+// 检查导出目录是否为空
+function checkExportPath() {
+    var path = $(".export-path-text").val();
+    if (path == ""){
+        return false;
+    }else {
+        return true;
+    }
+}
+// 检查是否添加文件
+function CheckFileList() {
+    var fileList = $(".file-list-ltem-checkbox").length;
+    if (fileList >0){
+        return true;
+    }else {
+        return false;
+    }
+}
+// 提取文件时进行检查
+function CheckFile() {
+    if (!CheckFileList()){
+        alert("您还未添加文件,请先添加文件后再进行提取操作")
+        return false;
+    }
+    if (!checkExportPath()){
+        if (!confirm("您还未填入导出路径,若没有指定导出目录，将默认导出到文件所在目录，是否继续？")){
+            $(".export-path-text").focus();
+            return false;
+        }
+    }
+    return true;
+}
